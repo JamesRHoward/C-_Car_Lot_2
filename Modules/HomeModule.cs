b24e8062601f841f84b0eae247cf87bda347b835
Bryant Wang and James Howard <bryant+james@>
2016-11-15 11:39:59 -0800

    Add search functionality to the program to find which cars fall within users inputted parameters.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -9,14 +9,31 @@ namespace CarDealership
     {
         public HomeModule()
         {
-            Get["/"] = _ => View["add_new_car.cshtml"];
+            Get["/"] = _ => View["index.cshtml"];
+            Get["/add-new-car"] = _ => View["add_new_car.cshtml"];
+            Get["/search-cars"] = _ => View["search_cars.cshtml"];
             Get["/view-all"] = _ => {
                 List<Car> allCars = Car.GetAll();
                 return View["view_all_cars.cshtml", allCars];
             };
+            Post["/view-searched"] = _ => {
+                int searchPrice = int.Parse(Request.Form["search-price"]);
+                int searchMileage = int.Parse(Request.Form["search-mileage"]);
+                List<Car> allCars = Car.GetAll();
+                List<Car> searchedCars = new List<Car> {};
+
+                foreach(Car thing in allCars)
+                {
+                    if(thing.GetPrice() <= searchPrice && thing.GetMiles() <= searchMileage)
+                    {
+                        searchedCars.Add(thing);
+                    }
+                }
+                return View["view_searched_cars.cshtml", searchedCars];
+            };
             Post["/car-added"] = _ => {
                 string carMake = Request.Form["car-make"];
-                int carPrice =  int.Parse(Request.Form["car-price"]);
+                int carPrice = int.Parse(Request.Form["car-price"]);
                 int carMiles = int.Parse(Request.Form["car-mileage"]);
                 Car newCar = new Car(carMake, carPrice, carMiles);
                 newCar.Save();
